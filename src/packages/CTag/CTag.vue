5423b4251c20d7b5cf57f99bbf78b968a7cb5f2d
Owen Tsai <owentsai.v@gmail.com>
2019-09-20 21:03:48 +0800

    Add: Tag

--- src/packages/CTag/CTag.vue
+++ src/packages/CTag/CTag.vue
@@ -1,15 +1,20 @@
 <template>
-  <span :class="[...c, ...[
-    {
-      'outlined': outlined,
-      'flat': flat,
-      'round': round,
-      'sharp': sharp
-    }
-  ]]" v-if="visible">
-    <slot></slot>
-    <c-icon icon-set="giraffe" v-if="!persistent" @click="handleClose">close</c-icon>
-  </span>
+  <transition name="c-zoom-in-center">
+    <span :class="[...c, ...[
+      {
+        'outlined': outlined,
+        'flat': flat,
+        'round': round,
+        'sharp': sharp
+      },
+      {
+        'clickable': clickable
+      }
+    ]]" @click="handleClick">
+      <slot></slot>
+      <c-icon icon-set="giraffe" v-if="closable" @click.native.stop="handleClose">close</c-icon>
+    </span>
+  </transition>
 </template>
 
 <script>
@@ -21,17 +26,15 @@
     components: {
       CIcon
     },
-    data: () => ({
-      visible: true
-    }),
     props: {
       color: String,
       size: String,
       outlined: Boolean,
       flat: Boolean,
-      persistent: Boolean,
+      closable: Boolean,
       round: Boolean,
-      sharp: Boolean
+      sharp: Boolean,
+      clickable: Boolean
     },
     computed: {
       c() {
@@ -43,8 +46,10 @@
     },
     methods: {
       handleClose() {
-        this.visible = false;
         this.$emit('close');
+      },
+      handleClick(ev) {
+        this.$emit('click', ev);
       }
     }
   }
